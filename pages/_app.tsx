4a2ad1759238280a273785a79020b2fafbe4f6b7
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2021-10-11 10:48:17 -0300

    fix: theme loading flashing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/app.css'
 import '@/styles/tailwind.css'
 import '@/styles/global.css'
 
-import { ThemeProvider } from '@/modules/ThemeProvider'
+import { ThemeProvider } from 'next-themes'
 
 import Navbar from '@/components/navbar'
 import SEO from '@/components/seo'
@@ -17,11 +17,18 @@ const meta: MetaTags = {
   siteName: 'Pedro Reis - Fullstack developer',
 }
 
+const THEME_STORAGE = '@ilher/theme'
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <SEO pageTitle="Pedro Reis - Fullstack developer" meta={meta} />
-      <ThemeProvider>
+      <ThemeProvider
+        attribute="class"
+        storageKey={THEME_STORAGE}
+        defaultTheme="dark"
+        enableSystem={false}
+      >
         <div className="min-h-screen">
           <Navbar />
           <Component {...pageProps} />
