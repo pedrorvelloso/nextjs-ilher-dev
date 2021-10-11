33fa497dcfd09fc04cc9a47f31eeeb659e25895b
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2021-10-11 18:09:42 -0300

    fix: stack margin

--- src/components/sections/stack-section.tsx
+++ src/components/sections/stack-section.tsx
@@ -66,7 +66,7 @@ const infraStack = `{
 function StackSection() {
   return (
     <div className="bg-gray-300 dark:bg-color-section py-8">
-      <Section className="grid grid-cols-1 gap-y-4 gap-x-4 xl:gap-x-8 lg:grid-cols-3 justify-items-center mx-0">
+      <Section className="grid grid-cols-1 gap-y-4 gap-x-4 xl:gap-x-8 lg:grid-cols-3 justify-items-center mx-0 lg:mx-auto">
         <CodeWindow title="frontend.json" code={frontendStack} />
         <CodeWindow title="backend.json" code={backendStack} />
         <CodeWindow title="infra.json" code={infraStack} />
