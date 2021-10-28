5c0826129db7afa15ce450add59e5f9e8e432f32
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2021-10-28 11:52:59 -0300

    fix: remove preconnect

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,11 +20,6 @@ class MyDocument extends Document {
             }}
           />
           <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="anonymous"
-          />
-          <link
             href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
