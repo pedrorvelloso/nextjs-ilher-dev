6180213ed245f02da111bd56ea929c18649722a0
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2021-10-28 11:35:54 -0300

    fix: revert font update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,7 +20,7 @@ class MyDocument extends Document {
             }}
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Nunito&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=optional"
             rel="stylesheet"
           />
           <link
